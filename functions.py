4f045c8b3bef6372914fabfc8729dd2938cc87a6
Kong Ren Hwai <kongrenhwai@hotmail.com>
2025-10-14 09:41:30 +0800

    Update functions.py
    update to gemini 2.5

--- functions.py
+++ functions.py
@@ -39,7 +39,7 @@ JOB DESCRIPTION: {job_summary}
   return prompt
 
 def gemini_chat(full_prompt):
-    model = genai.GenerativeModel('gemini-2.0-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
     answer = model.generate_content(full_prompt)
     for chunk in answer.text:
         yield chunk
